58be4a92430bab1828680f89c26a27947184d85b
Kartikk R Pandian <kartikk127@gmail.com>
2017-03-22 21:00:55 -0400

    Moved add book from menu to FAB

--- app/src/main/java/com/kartikk/prolificapp/prolificapp/MainActivity.java
+++ app/src/main/java/com/kartikk/prolificapp/prolificapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.kartikk.prolificapp.prolificapp;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +12,14 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.crashlytics.android.Crashlytics;
+import com.facebook.rebound.SimpleSpringListener;
+import com.facebook.rebound.Spring;
+import com.facebook.rebound.SpringConfig;
+import com.facebook.rebound.SpringSystem;
 import com.kartikk.prolificapp.prolificapp.databinding.ActivityMainBinding;
 import com.kartikk.prolificapp.prolificapp.models.Book;
 import com.kartikk.prolificapp.prolificapp.util.Helper;
@@ -48,16 +54,41 @@ public class MainActivity extends AppCompatActivity {
         context = this;
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         recyclerView = activityMainBinding.booksRecyclerView;
-
+        final FloatingActionButton fab = activityMainBinding.mainFAB;
+        SpringSystem springSystem = SpringSystem.create();
+        SpringConfig springConfig = new SpringConfig(800, 15);
+        final Spring spring = springSystem.createSpring();
+        spring.setSpringConfig(springConfig);
+        spring.addListener(new SimpleSpringListener() {
+            @Override
+            public void onSpringUpdate(Spring spring) {
+                float value = (float) spring.getCurrentValue();
+                float scale = 1f - (value * 0.5f);
+                fab.setScaleX(scale);
+                fab.setScaleY(scale);
+            }
+        });
+        fab.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        spring.setEndValue(1f);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        spring.setEndValue(0f);
+                        Intent addIntent = new Intent(getApplicationContext(), AddBookActivity.class);
+                        startActivity(addIntent);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_add:
-                Intent addIntent = new Intent(this, AddBookActivity.class);
-                startActivity(addIntent);
-                return true;
             case R.id.menu_delete_all:
                 Call<Void> call = Helper.getRetrofitEndpoints().deleteAllBooks();
                 call.enqueue(new Callback<Void>() {
